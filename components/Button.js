5f5866d88029989b604f975702343e94127c8802
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2023-04-15 11:40:35 -0300

    update media query units

--- components/Button.js
+++ components/Button.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Button extends React.Component {
     render() {
       return (
-      <div className="w-40 place-self-end overflow-visible p-2.5">
-        <button id="new-quote" className="active:animate-click hover:bg-gray-300/40 sm:text-md sm:p-3 sm:w-24 text-white bg-white/20 border-solid border-1 border-gray-500/10 drop-shadow rounded text-sm p-2.5 mb-1 w-24 leading-2" onClick={this.props.handleChange}>New quote</button>
+      <div className="w-40 3xl:w-60 place-self-end overflow-visible p-2.5">
+        <button id="new-quote" className="active:animate-click hover:bg-gray-300/40 3xl:text-xl 2xl:w-full sm:text-md sm:p-3 sm:w-24 text-white bg-white/20 border-solid border-1 border-gray-500/10 drop-shadow rounded text-sm p-2.5 mb-1 w-24 leading-2" onClick={this.props.handleChange}>New quote</button>
       </div>
       );
     }
--- components/Quote.js
+++ components/Quote.js
@@ -4,7 +4,7 @@ class Quote extends React.Component {
   render() {
     return (
     <div className="bg-black/70 text-white px-7 py-5 rounded-t-xl col-start-1 col-end-11 row-start-1 row-end-7" id="text">
-      <h3 id="phrase"><i className="fas fa-quote-left fa-sm" style={{
+      <h3 id="phrase" className="3xl:text-2xl"><i className="fas fa-quote-left fa-sm" style={{
           color: "white",
           textShadow: "0.1 1 1 rgba(0, 0, 0, 0.486)"
           }}></i>&nbsp;&nbsp;{this.props.quote}</h3>
