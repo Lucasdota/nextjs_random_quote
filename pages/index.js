de05eb0742055877da710cbbf47566ae5f110424
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2023-04-15 12:38:35 -0300

    update bg

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/mark.svg" />
       </Head>
       <main className="bg-[url('../public/images/cozyrest.jpg')] bg-no-repeat bg-cover bg-center bg-fixed text-center w-screen h-screen flex justify-center items-center">
-        <div className='w-80 sm:w-4/5 md:w-3/5 lg:w-2/4 xl:w-1/4 sm:px-5 sm:py-5 mx-2 px-4 py-4 bg-gray-600/20 backdrop-blur rounded grid grid-rows-8 grid-cols-10 shadow-[inset_0_0px_45px_15px_rgba(0,0,0,.4)]' id='quote-box'>
+        <div className='w-80 sm:w-4/5 md:w-3/5 lg:w-2/4 xl:w-1/4 sm:px-5 sm:py-5 mx-2 px-4 py-4 bg-gray-600/20 backdrop-blur rounded-lg grid grid-rows-8 grid-cols-10 shadow-[inset_0_0px_45px_15px_rgba(0,0,0,.4)]' id='quote-box'>
           <GetQuote />
         </div>
         <footer className="absolute top-[96%] w-full text-right pr-4">
